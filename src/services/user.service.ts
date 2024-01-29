cb4b70e5593027acf45f4862cb080e766c3c57be
jack ning <github@bytedesk.com>
2024-01-29 12:05:15 +0800

    update

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,7 +1,15 @@
+/*
+ * @Author: jack ning github@bytedesk.com
+ * @Date: 2024-01-29 11:50:09
+ * @LastEditors: jack ning github@bytedesk.com
+ * @LastEditTime: 2024-01-29 11:53:05
+ * @FilePath: /react-typescript-authentication-example/src/services/user.service.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import axios from "axios";
 import authHeader from "./auth-header";
 
-const API_URL = "http://localhost:8080/api/test/";
+const API_URL = "http://localhost:1001/api/test/";
 
 export const getPublicContent = () => {
   return axios.get(API_URL + "all");
